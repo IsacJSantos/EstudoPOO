5855e86a464504875a56b3025b4f9d692bb9108a
Isac <isac4322@gmail.com>
2021-02-03 21:17:16 -0300

    Nullable

---
+++ Projetos/Seção6/Nullable/Nullable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = 0;
+            double? y = x ?? 0.0;
+
+            Console.WriteLine(y);
+
+        }
+    }
+}
