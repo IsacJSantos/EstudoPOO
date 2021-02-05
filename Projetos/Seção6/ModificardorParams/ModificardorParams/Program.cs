756876c87ef152c9ef3d7bea6cc706579ba5e91a
Isac <isac4322@gmail.com>
2021-02-05 17:08:24 -0300

    Parameter modifier Params

---
+++ Projetos/Seção6/ModificardorParams/ModificardorParams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ModificardorParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = Caculator.Sum(3, 2);
+            int sum2 = Caculator.Sum(1, 2, 10);
+            int sum3 = Caculator.Sum(1, 2, 5, 6, 1);
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+        }
+    }
+}
