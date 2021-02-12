3390c6d4819f1c317cb0d27ff6b97263a95ead15
Isac <isac4322@gmail.com>
2021-02-12 16:51:03 -0300

    TernaryOperator

---
+++ Projetos/Seção7/TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = double.Parse(Console.ReadLine());
+            double discount = (price >= 20) ? price * 0.1 : price * 0.05;
+
+            Console.WriteLine(discount.ToString("F2"));
+
+        }
+    }
+}
