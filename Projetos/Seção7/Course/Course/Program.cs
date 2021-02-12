aeb411918335c118b60569fdce447bf703ae8af4
Isac <isac4322@gmail.com>
2021-02-12 16:31:17 -0300

    Type inference

--- Projetos/Seção7/Course/Course/Program.cs
+++ Projetos/Seção7/Course/Course/Program.cs
@@ -6,8 +6,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Point p = new Point(18,15);
-            Console.WriteLine(p);
+            var x = 5;
+            var y = 5.6;
+            var z = 8.55f;
+            var w = "Isac";
+
+            Console.WriteLine(x.GetType() + ": " + x);
+            Console.WriteLine(y.GetType() + ": " + y);
+            Console.WriteLine(z.GetType() + ": " + z);
+            Console.WriteLine(w.GetType() + ": " + w);
         }
     }
 }
