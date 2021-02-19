24fb93f0c7d81fa5fc36f09d42d88bc398d80ac8
Isac <isac4322@gmail.com>
2021-02-19 15:22:44 -0300

    Enum order project
    I learned a lot of things about enums

---
+++ Projetos/Seção9/EnumProject/EnumProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using EnumProject.Entities;
+using EnumProject.Entities.Enums;
+
+namespace EnumProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.Shipped
+            };
+
+            Console.WriteLine(order);
+            Console.WriteLine("-----------------------------------");
+            string txt = OrderStatus.Processing.ToString();
+
+            Console.WriteLine("Convert enum to string: " + txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine("Covert string to enum: " + os);
+
+            OrderStatus os2 = (OrderStatus)2;
+
+            Console.WriteLine("Convert int to enum (with cast): " + os2);
+        }
+    }
+}
--- Projetos/Seção9/EnumProject/EnumProject/obj/Debug/netcoreapp3.1/EnumProject.AssemblyInfo.cs
+++ Projetos/Seção9/EnumProject/EnumProject/obj/Debug/netcoreapp3.1/EnumProject.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("EnumProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Teste")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
+[assembly: System.Reflection.AssemblyProductAttribute("EnumProject")]
+[assembly: System.Reflection.AssemblyTitleAttribute("EnumProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
