e78b5189b4d9a95261567d518543aa27ad497ea1
Isac <isac4322@gmail.com>
2021-02-23 15:07:41 -0300

    Upcasting and DownCasting

---
+++ Projetos/Seção10/InheritanceCourse/InheritanceCourse/Entities/SavingsAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceCourse.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestrate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestrate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
