adaca13163bd1aa2c0c3cf125c22f59dd7c2599b
Isac <isac4322@gmail.com>
2021-02-23 13:31:54 -0300

    Create folder Secao10 and inheritance project

---
+++ Projetos/Seção10/InheritanceCourse/InheritanceCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InheritanceCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
