322acc291636e9f61d026bb6f933bc89d2eaaa78
Isac <isac4322@gmail.com>
2021-02-24 15:49:54 -0300

    Abstract Classes

---
+++ Projetos/Seção10/AbstractClasses/AbstractClasses/Entities/BusinessAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace AbstractClasses.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number,holder,balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount) 
+        {
+            if(amount <= LoanLimit)
+            Balance += amount;
+        }
+
+        public override void WithDraw(double amount)
+        {
+            base.WithDraw(amount);
+            Balance -= 3;
+        }
+    }
+}
---
+++ Projetos/Seção10/AbstractClasses/AbstractClasses/Entities/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClasses.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestrate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestrate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void WithDraw(double amount)
+        {
+            base.WithDraw(amount);
+            Balance -= 7;
+        }
+
+    }
+}
