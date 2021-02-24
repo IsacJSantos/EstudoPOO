5c7931007a6c63ae9cf3d00ea37eb2cd07ca96e9
Isac <isac4322@gmail.com>
2021-02-24 17:22:54 -0300

    Complete linkedList setup

---
+++ Projetos/Especial/LinkedListProject/LinkedListProject/Entities/Node.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProject.Entities
+{
+    class Node
+    {
+        private double Value;
+        private Node Next;
+
+        public Node(double value)
+        {
+            Value = value;
+        }
+
+        public void SetValue(double value)
+        {
+            Value = value;
+        }
+
+        public double GetValue()
+        {
+            return Value;
+        }
+
+        public void SetNext(Node next)
+        {
+            Next = next;
+        }
+
+        public Node GetNext()
+        {
+            return Next;
+        }
+
+    }
+}
---
+++ Projetos/Especial/LinkedListProject/LinkedListProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LinkedListProject.Entities;
+
+namespace LinkedListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            list.Add(3);
+            Console.WriteLine(list);
+            list.Add(5);
+            Console.WriteLine(list);
+            list.Add(7);
+            Console.WriteLine(list);
+            list.Add(6);
+            Console.WriteLine(list);
+        }
+    }
+}
