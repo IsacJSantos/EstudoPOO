778f27f00429b1eaf05d45f1f5dc9361d6b0d7e3
Isac <isac4322@gmail.com>
2021-02-24 13:02:48 -0300

    Sealed Classes and methods

---
+++ Projetos/Seção10/SealedClassesAndMethods/SealedClassesAndMethods/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SealedClassesAndMethods.Entities
+{
+    class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual  void  WithDraw(double amount) 
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) 
+        {
+            Balance += amount;
+        }
+
+    }
+}
