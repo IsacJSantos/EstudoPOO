15459ba1e45ad8f7c61b6d863c23475ae4eaa295
Isac <isac4322@gmail.com>
2021-02-24 17:39:04 -0300

    Finish LinkedList Menu logic

--- Projetos/Especial/LinkedListProject/LinkedListProject/Program.cs
+++ Projetos/Especial/LinkedListProject/LinkedListProject/Program.cs
@@ -9,14 +9,36 @@ namespace LinkedListProject
         {
             LinkedList list = new LinkedList();
 
-            list.Add(3);
-            Console.WriteLine(list);
-            list.Add(5);
-            Console.WriteLine(list);
-            list.Add(7);
-            Console.WriteLine(list);
-            list.Add(6);
-            Console.WriteLine(list);
+            int option;
+            do
+            {
+                ShowMenu();
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Write("Enter a number: ");
+                        double number = double.Parse(Console.ReadLine());
+                        list.Add(number);
+                        break;
+                    case 2:
+                        Console.WriteLine(list);
+                        break;
+                    default:
+                        Console.WriteLine("Ending program...");
+                        break;
+                }
+
+            } while (option != 3 );
+            
+        }
+
+        static void ShowMenu() 
+        {
+            Console.WriteLine("1 - Add a new element");
+            Console.WriteLine("2 - Show the list");
+            Console.WriteLine("3 - Exit");
         }
     }
 }
