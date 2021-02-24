76631f0d6205d51001c344025cd714d1d4855663
Isac <isac4322@gmail.com>
2021-02-24 15:10:55 -0300

    Inheritense and polymorphysm exercise

---
+++ Projetos/Seção10/Exercicios/Exercicio1/ProductManager/ProductManager/Entities/UsedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManager.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manuFactureDate)
+        : base(name, price)
+        {
+            ManufactureDate = manuFactureDate;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return Name 
+                + " (used) $ " 
+                + Price.ToString("F2",CultureInfo.InvariantCulture)
+                + $" (Manufactured date: {ManufactureDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
