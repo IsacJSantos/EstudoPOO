75871399f875fe2f2990165aa119ab023c1f4a1d
Isac <isac4322@gmail.com>
2021-02-26 16:01:46 -0300

    Exercise solved with success

---
+++ Projetos/Section10/Exercicios/Exercicio1/TaxesCalculator/TaxesCalculator/Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxesCalculator.Entities
+{
+    class Company : TaxPayers
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company(string name, double anualIcome, int numberOfEmployees)
+        :base(name,anualIcome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Taxes()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
---
+++ Projetos/Section10/Exercicios/Exercicio1/TaxesCalculator/TaxesCalculator/Entities/Individual.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaxesCalculator.Entities
+{
+    class Individual: TaxPayers
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIcome, double healthExpenditures)
+        : base(name, anualIcome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Taxes()
+        {
+            if (AnualIncome < 20000.0)
+            {
+                double tax = AnualIncome * 0.15;
+                if (HealthExpenditures > 0)
+                {
+                    return tax - (HealthExpenditures * 0.5);
+                }
+                return tax;
+            }
+            else 
+            {
+                double tax = AnualIncome * 0.25;
+                if (HealthExpenditures > 0)
+                {
+                    return tax - (HealthExpenditures * 0.5);
+                }
+                return tax;
+            }
+        }
+
+    }
+}
