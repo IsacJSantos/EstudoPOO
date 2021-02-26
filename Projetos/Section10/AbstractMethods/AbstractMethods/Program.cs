72751fbd90e16729a5dbf265e5062a7b662024e8
Isac <isac4322@gmail.com>
2021-02-26 14:17:24 -0300

    Creat abstract methods project

---
+++ Projetos/Section10/AbstractMethods/AbstractMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
