fc61fadd4dda91e7ff70ddc795bbd31fe586bd53
Isac <isac4322@gmail.com>
2021-03-16 19:36:12 -0300

    Complete exenption exercise

---
+++ Projetos/Section10/Exercicios/Exercicio2/ExenptionExercise/ExenptionExercise/Entities/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ExenptionExercise.Entities.Exenptions;
+
+namespace ExenptionExercise.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account(int number, string holder, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            WithDrawLimit = withDrawLimit;
+        }
+        public Account(int number, string holder, double balance, double withDrawLimit) : this(number, holder, balance)
+        {
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount) 
+        {
+            if(amount > 0)
+            Balance += amount;
+        }
+        public void WithDraw(double amount) 
+        {
+            if (amount > WithDrawLimit)
+            {
+                throw new DomainExenption("Not enough balance!");
+            }
+            else if (Balance < amount) 
+            {
+                throw new DomainExenption("The amout exceeds withdraw limit!");
+            }
+
+            Balance -= amount;
+        }
+
+    }
+}
---
+++ Projetos/Section10/Exercicios/Exercicio2/ExenptionExercise/ExenptionExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using ExenptionExercise.Entities.Exenptions;
+using ExenptionExercise.Entities;
+
+namespace ExenptionExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ener account data: ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double balance = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("WithDraw Limit: ");
+            double withDrawLimit = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Account ac = new Account(number, holder, withDrawLimit);
+            ac.Deposit(balance);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            try
+            {
+                ac.WithDraw(amount);
+                Console.WriteLine("New Balance: " + ac.Balance.ToString("F2"), CultureInfo.InvariantCulture);
+            }
+            catch (DomainExenption e)
+            {
+                Console.WriteLine("WithDrow error: " + e.Message);
+            }
+            catch (FormatException e) 
+            {
+                Console.WriteLine("Format Error: " + e.Message);
+            }
+
+           
+
+        }
+    }
+}
