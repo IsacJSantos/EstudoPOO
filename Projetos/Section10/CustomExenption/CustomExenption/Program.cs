13c1ed2a9c43c67dfb908ff0d5c443ef4846c162
Isac <isac4322@gmail.com>
2021-03-16 13:24:40 -0300

    Good solution

--- Projetos/Section10/CustomExenption/CustomExenption/Program.cs
+++ Projetos/Section10/CustomExenption/CustomExenption/Program.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using CustomExenption.Entities;
+using CustomExenption.Entities.Exeptions;
+
 namespace CustomExenption
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Room number: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.Write("Check-in date (dd/MM/yyyy): ");
-            DateTime checkIn = DateTime.Parse(Console.ReadLine());
-            Console.Write("Check-out date (dd/MM/yyyy): ");
-            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-            if (checkOut < checkIn)
-            {
-                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
-            }
-            else
-            {
+
                 Reservation reservation = new Reservation(number, checkIn, checkOut);
                 Console.WriteLine("Reservation: " + reservation);
                 Console.WriteLine();
@@ -29,15 +28,12 @@ namespace CustomExenption
                 Console.Write("Check-out date (dd/MM/yyyy): ");
                 checkOut = DateTime.Parse(Console.ReadLine());
 
-                string error = reservation.UpdateDates(checkIn, checkOut);
-                if (error != null)
-                {
-                    Console.WriteLine("Error in reservation: " + error);
-                }
-                else
-                {
-                    Console.WriteLine("Reservation: " + reservation);
-                }
+
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
             }
         }
     }
