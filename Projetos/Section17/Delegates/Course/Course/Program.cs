25fdd774eaa7d89a76eda56bf66b4454ecdc8344
Isac <isac4322@gmail.com>
2021-04-05 10:50:56 -0300

    Delegates Introduction

---
+++ Projetos/Section17/Delegates/Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Course.Services;
+namespace Course
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op.Invoke(a,b);
+            Console.WriteLine(result);
+        }
+    }
+}
