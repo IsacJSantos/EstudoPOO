d3268934351a4725154d89409a231bf21a9b50ef
Isac <isac4322@gmail.com>
2021-04-07 11:01:23 -0300

    Creating and writing files with File and FileInfo

---
+++ Projetos/Section13/FileFileInfo/Course/Course/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+                throw;
+            }
+        }
+    }
+}
