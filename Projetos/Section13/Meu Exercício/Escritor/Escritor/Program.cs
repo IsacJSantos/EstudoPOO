4cdb69e5a37f50936ea406c2c450f0be13e829a2
Isac <isac4322@gmail.com>
2021-04-13 11:15:16 -0300

    Write async

---
+++ Projetos/Section13/Meu Exercício/Escritor/Escritor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Escritor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        char[] cArray = line.ToCharArray();
+                        WriteAsync(cArray);
+                        Console.ReadLine();
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static async void WriteAsync(char[] cArray)
+        {
+            for (int i = 0; i < cArray.Length; i++)
+            {
+                await Task.Delay(30);
+                Console.Write(cArray[i]);
+
+            }
+
+        }
+    }
+}
