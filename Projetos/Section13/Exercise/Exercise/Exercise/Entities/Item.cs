ef886f82c35a169c23e07c2ad05e00c958ac2662
Isac <isac4322@gmail.com>
2021-04-22 11:05:11 -0300

    Exercise

---
+++ Projetos/Section13/Exercise/Exercise/Exercise/Entities/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercise.Entities
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public Item(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        double TotalPrice() 
+        {
+            return Price * Amount;
+        }
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + TotalPrice().ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Projetos/Section13/Exercise/Exercise/Exercise/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Exercise.Entities;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> items = new List<Item>();
+            string sourceCsv = @"C:\Users\isacj\Desktop\PaymentList\Source.csv";
+
+            using (StreamReader sr = File.OpenText(sourceCsv))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] s = sr.ReadLine().Split(',');
+                    string name = s[0];
+                    double price = double.Parse(s[1]);
+                    string s1 = s[2];
+                    int amount = int.Parse(s1);
+
+                    Item item = new Item(name, price, amount);
+                    items.Add(item);
+                }
+            }
+
+            try
+            {
+                string summaryPath = Path.GetDirectoryName(sourceCsv) + @"\out";
+                string summaryCsv = summaryPath + @"\Summary.csv";
+                Directory.CreateDirectory(summaryPath);
+
+                using (StreamWriter sw = File.AppendText(summaryCsv))
+                {
+                    foreach (var item in items)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+
+        }
+    }
+}
