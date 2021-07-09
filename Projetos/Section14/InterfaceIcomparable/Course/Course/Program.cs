d9f36228cc1ac8a0f47aaee596d2e6a5a72417e2
Isac <isac4322@gmail.com>
2021-07-09 11:05:44 -0300

    using IComparable interfaces

---
+++ Projetos/Section14/InterfaceIcomparable/Course/Course/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path)) 
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                throw;
+            }
+        }
+    }
+}
