325fa788b0425c77bacdfb51b4e969eaf7089cc0
Isac <isac4322@gmail.com>
2021-08-16 11:07:39 -0300

    IComparable

---
+++ Revisao/rev/rev/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using rev.Entities;
+
+namespace rev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An Error occurred");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+    }
+}
