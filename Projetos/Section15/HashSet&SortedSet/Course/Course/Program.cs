96ec63fadb1547d670e15f5e06778d47c7a48bc5
Isac <isac4322@gmail.com>
2021-09-06 12:02:18 -0300

    HashSet and SortedSet part 1

---
+++ Projetos/Section15/HashSet&SortedSet/Course/Course/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 5, 6, 4, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 2, 4 };
+
+
+            PrintCollection<int>(a);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection) 
+        {
+            foreach (var item in collection)
+            {
+                Console.Write(item + " ");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
