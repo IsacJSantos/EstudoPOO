b84f391ca9927174d63ecdc797297d86a46d6c7b
Isac <isac4322@gmail.com>
2021-10-22 11:45:18 -0300

    Hashset exercise

---
+++ Projetos/Section15/HashSetExercise/LogReader/LogReader/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace LogReader.Entities
+{
+    class User
+    {
+        public string Name { get; set; }
+        public DateTime Datelog { get; set; }
+        public User(string name, DateTime timelog) 
+        {
+            Name = name;
+            Datelog = timelog;
+        }
+
+        public override string ToString()
+        {
+            return $"User name: {Name}. Log date: {Datelog}";
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is User)) 
+            {
+                Console.WriteLine("The obj is not a User");
+                return false;
+            }
+
+            User user = obj as User;
+            return user.Name == Name;
+
+        }
+    }
+}
---
+++ Projetos/Section15/HashSetExercise/LogReader/LogReader/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using LogReader.Entities;
+
+namespace LogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file path: ");
+            string path = Console.ReadLine();
+
+            HashSet<User> userlist = new HashSet<User>();
+            
+            try
+            {
+                using (StreamReader _sr = File.OpenText(path))
+                {
+                    while (!_sr.EndOfStream)
+                    {
+                        string[] s = _sr.ReadLine().Split(' ');
+                        DateTime dateLog = DateTime.Parse(s[1]);
+                        string name = s[0];
+                        User user = new User(name, dateLog);
+                        userlist.Add(user);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            Console.WriteLine($"Total users: {userlist.Count}");
+        }
+    }
+}
