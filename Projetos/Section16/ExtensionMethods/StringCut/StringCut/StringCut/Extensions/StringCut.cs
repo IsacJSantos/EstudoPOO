31e3d4daeaf606917a36deca812f9c0a7d425a13
Isac <isac4322@gmail.com>
2021-10-29 11:04:00 -0300

    Creating a Extended method to cut a String

---
+++ Projetos/Section16/ExtensionMethods/StringCut/StringCut/StringCut/Extensions/StringCut.cs
@@ -0,0 +1,13 @@
+﻿namespace System
+{
+    static class StringCut
+    {
+        public static string Cut(this String thisObj, int count)
+        {
+            if (thisObj.Length < count)
+                return thisObj;
+            else
+                return thisObj.Substring(0, count) + "...";
+        }
+    }
+}
