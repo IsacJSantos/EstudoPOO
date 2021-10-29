8b110f832aa21241df51981c2953d91b74fdb66b
Isac <isac4322@gmail.com>
2021-10-29 10:47:08 -0300

    Creating a extension method to DateTime

---
+++ Projetos/Section16/ExtensionMethods/ElapsedTime/ElapsedTime/Extensios/DateTimeExtensios.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    static class DateTimeExtensios
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalSeconds < 60)
+                return duration.TotalSeconds.ToString("F1", CultureInfo.InvariantCulture) + " seconds";
+            else if (duration.TotalMinutes < 60)
+                return duration.TotalMinutes.ToString("F1", CultureInfo.InvariantCulture) + " minutes";
+            else if (duration.TotalHours < 24)
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            else
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days";
+
+        }
+    }
+}
---
+++ Projetos/Section16/ExtensionMethods/ElapsedTime/ElapsedTime/ProcessFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ElapsedTime
+{
+    public class ProcessFile
+    {
+        public static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 10, 29, 10, 46,0);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
