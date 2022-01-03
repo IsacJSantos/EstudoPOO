06cf00d54eafe175035d8adbd991b930abb1c955
Isac <isac4322@gmail.com>
2022-01-03 11:36:34 -0300

    Using delegates

--- Projects/Section17/Delegates/Course/Course/Program.cs
+++ Projects/Section17/Delegates/Course/Course/Program.cs
@@ -1,34 +1,22 @@
 ﻿using System;
 using Course.Services;
+
 namespace Course
 {
     class Program
     {
-        delegate void BinaryNumericOperation(double n1, double n2);
+        delegate double BinaryNumericOperation(double n1, double n2);
+
         static void Main(string[] args)
         {
             double a = 10;
-            double b = 12;
+            double b = 5;
 
-            BinaryNumericOperation op = Teste;
-            op += Teste2;
-            op += Teste3;
+            BinaryNumericOperation op = CalculationService.Sum;
 
-            op.Invoke(a, b);
-           
-        }
+            double result = op.Invoke(a, b);
 
-        public static void Teste(double n1, double n2) 
-        {
-            Console.WriteLine("Testando...: " + n1 + n2);
-        }
-        public static void Teste2(double n1, double n2)
-        {
-            Console.WriteLine("Testando2...: " + n1 + n2);
-        }
-        public static void Teste3(double n1, double n2)
-        {
-            Console.WriteLine("Testando3...: " + n1 + n2);
+            Console.WriteLine(result);
         }
     }
 }
