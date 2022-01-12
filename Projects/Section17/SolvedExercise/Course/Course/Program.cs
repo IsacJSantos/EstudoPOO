dbb752b4b136e99090148a159f1725dbafb6ad2e
Isac <isac4322@gmail.com>
2022-01-12 08:58:33 -0300

    reading csv file and using LINQ

---
+++ Projects/Section17/SolvedExercise/Course/Course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    list.Add(new Product(fields[0], double.Parse(fields[1], CultureInfo.InvariantCulture)));
+                }
+            }
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0).Average();
+            Console.WriteLine("Avarage price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
