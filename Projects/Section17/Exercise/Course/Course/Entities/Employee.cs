5b621459cd2570f943beccefd2de3ce29cf0b8cd
Isac <isac4322@gmail.com>
2022-01-12 09:32:06 -0300

    Solving the exercise

---
+++ Projects/Section17/Exercise/Course/Course/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace Course.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            this.salary = salary;
+        }
+    }
+}
---
+++ Projects/Section17/Exercise/Course/Course/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using Course.Entities;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double inputSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] s = sr.ReadLine().Split(',');
+                    string name = s[0];
+                    string email = s[1];
+                    double salary = double.Parse(s[2], CultureInfo.InvariantCulture);
+                    list.Add(new Employee(name, email, salary));
+                }
+            }
+            Console.WriteLine($"Email of people whose salary is more than {inputSalary}: ");
+            var emails = list.Where(e => e.salary > inputSalary).OrderBy(e => e.Email).Select(e => e.Email);
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+            Console.WriteLine();
+
+            var sum = list.Where(e => e.Name[0] == 'M').Select(e => e.salary).Sum();
+            Console.WriteLine($"Sum of salary of people whose name starts with 'M': {sum.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
